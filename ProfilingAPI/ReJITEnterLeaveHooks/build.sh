08541df5f8f93613c896a9f8a163627698b4fad4
Nigel Delaney <spamavoid@outlook.com>
2017-11-03 23:32:31 -0700

    Remove compiler specific flag

--- ProfilingAPI/ReJITEnterLeaveHooks/build.sh
+++ ProfilingAPI/ReJITEnterLeaveHooks/build.sh
@@ -13,8 +13,14 @@ printf '  BuildType    : %s\n' "$BuildType"
 
 printf '  Building %s ... ' "$Output"
 
-CXX_FLAGS="$CXX_FLAGS --no-undefined -Wno-invalid-noreturn -fPIC -fms-extensions -DBIT64 -DPAL_STDCPP_COMPAT -DPLATFORM_UNIX -std=c++11"
+CXX_FLAGS="$CXX_FLAGS -Wno-invalid-noreturn -fPIC -fms-extensions -DBIT64 -DPAL_STDCPP_COMPAT -DPLATFORM_UNIX -std=c++11"
 INCLUDES="-I $CORECLR_PATH/src/pal/inc/rt -I $CORECLR_PATH/src/pal/prebuilt/inc -I $CORECLR_PATH/src/pal/inc -I $CORECLR_PATH/src/inc -I $CORECLR_PATH/bin/Product/$BuildOS.$BuildArch.$BuildType/inc"
+
+if [ $BuildOS="MacOSX" ]; then
+	CXX_FLAGS+=" -Wl,-undefined,error"
+else
+	CXX_FLAGS+=" --no-undefined"
+fi
 
 clang++ -shared -o $Output $CXX_FLAGS $INCLUDES ClassFactory.cpp CorProfiler.cpp dllmain.cpp ILRewriter.cpp
 
